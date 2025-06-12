be88c1601ab3fd7af42a8d8ae0d0882d971d791d
ValentinaDelgadoRincon <valentinadr1403@gmail.com>
2025-06-12 09:13:16 -0500

    feat: :sparkles: create js search
    create js search
    
    By @Valentina

--- componentes/main.js
+++ componentes/main.js
@@ -23,3 +23,46 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+class barraBuscador extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode:"open"});
+        this.shadowRoot.innerHTML = `
+        <input type="text" placeholder="Buscar" />
+        <style>
+          input {
+            display:flex;
+            padding: 10px;
+            font-size: 16px;
+            width: 100%;
+            max-width: 70%;
+            margin-bottom: 20px;
+            font-size: 1.7rem;
+            color:black;
+            justify-content: center;
+            display: flex;
+            height: 30%;
+            margin-top: 8vh;
+            margin-left: 15%;
+            align-items: center;
+            background-color: #3e565994;
+            border-radius: 10px;
+          }
+        </style>
+      `;
+    }
+
+    connectedCallback(){
+        const input = this.shadowRoot.querySelector("input");
+        input.addEventListener ("input", () => {
+            const eventoContenido = new CustomEvent("busqueda", {
+                detail:input.value.toLowerCase(),
+                bubbles: true,
+                composed: true
+            });
+            this.dispatchEvent(eventoContenido);
+        })
+    }
+}
+customElements.define("barra-buscador", barraBuscador);
